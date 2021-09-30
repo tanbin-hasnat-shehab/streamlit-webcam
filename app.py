2159fefc0523ad3e46d3d7ad4a636ea9f0a0e0aa
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-09-30 13:53:07 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import cv2
+
+run=st.checkbox('run')
+frame_window=st.image([])
+cam=cv2.VideoCapture(0)
+while run:
+	ret,frame=cam.read()
+	frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	frame_window.image(frame)
+	frame_window.image(frame)
+else:
+	st.write('stopped')
